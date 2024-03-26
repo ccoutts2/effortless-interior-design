08585924b20fee6cf9a01553c8728dae11469b59
Chris Coutts <chris.dcoutts@gmail.com>
2024-03-26 12:26:06 +0000

    Add search bar functionality

--- src/components/header/NavBar/NavBar.tsx
+++ src/components/header/NavBar/NavBar.tsx
@@ -21,64 +21,67 @@ const NavBar = () => {
 
   return (
     <main className={styles.header}>
-      <div className={styles.bar}>
-        <div
-          onMouseDown={() => {
-            setIsActive(!isActive);
-          }}
-          className={styles.el}>
+      {!isVisible && (
+        <div className={styles.bar}>
           <div
-            className={`${styles.burger} ${
-              isActive ? styles.burgerActive : ""
-            }`}></div>
-          <div className={styles.label}>
-            <motion.p variants={opacity} animate={isActive ? "closed" : "open"}>
-              menu
-            </motion.p>
-            <motion.p variants={opacity} animate={!isActive ? "closed" : "open"}>
-              close
-            </motion.p>
+            onMouseDown={() => {
+              setIsActive(!isActive);
+            }}
+            className={styles.el}>
+            <div
+              className={`${styles.burger} ${
+                isActive ? styles.burgerActive : ""
+              }`}></div>
+            <div className={styles.label}>
+              <motion.p variants={opacity} animate={isActive ? "closed" : "open"}>
+                menu
+              </motion.p>
+              <motion.p variants={opacity} animate={!isActive ? "closed" : "open"}>
+                close
+              </motion.p>
+            </div>
           </div>
+          <div className={styles.desktopEl}>
+            <div onMouseEnter={() => setIsShopActive(true)} className={styles.nav}>
+              <Link href="/shop"> shop</Link>
+            </div>
+            <div onMouseEnter={() => setIsShopActive(false)} className={styles.nav}>
+              <Link href="/workshops"> workshops</Link>
+            </div>
+            <div onMouseEnter={() => setIsShopActive(false)} className={styles.nav}>
+              <Link className={``} href="/consultations">
+                {" "}
+                consultations
+              </Link>
+            </div>
+          </div>
+          <div onMouseEnter={() => setIsShopActive(false)} className={styles.logo}>
+            <Link href="/">effortless interior design</Link>
+          </div>
+          <div
+            onMouseEnter={() => setIsShopActive(false)}
+            className={styles.desktopEl}>
+            <div className={styles.nav}>
+              <Link href="/resources"> resources</Link>
+            </div>
+            <div className={styles.nav}>
+              <Link href="/about"> about</Link>
+            </div>
+            <div className={styles.nav}>
+              <Link href="/contact"> contact</Link>
+            </div>
+          </div>
+          <motion.div
+            variants={opacity}
+            animate={isActive ? "closed" : "open"}
+            className={styles.shopContainer}>
+            <FiShoppingCart />
+            <IoIosSearch onClick={toggleSearchBar} />
+          </motion.div>
         </div>
-        <div className={styles.desktopEl}>
-          <div onMouseEnter={() => setIsShopActive(true)} className={styles.nav}>
-            <Link href="/shop"> shop</Link>
-          </div>
-          <div onMouseEnter={() => setIsShopActive(false)} className={styles.nav}>
-            <Link href="/workshops"> workshops</Link>
-          </div>
-          <div onMouseEnter={() => setIsShopActive(false)} className={styles.nav}>
-            <Link className={``} href="/consultations">
-              {" "}
-              consultations
-            </Link>
-          </div>
-        </div>
-        <div onMouseEnter={() => setIsShopActive(false)} className={styles.logo}>
-          <Link href="/">effortless interior design</Link>
-        </div>
-        <div
-          onMouseEnter={() => setIsShopActive(false)}
-          className={styles.desktopEl}>
-          <div className={styles.nav}>
-            <Link href="/resources"> resources</Link>
-          </div>
-          <div className={styles.nav}>
-            <Link href="/about"> about</Link>
-          </div>
-          <div className={styles.nav}>
-            <Link href="/contact"> contact</Link>
-          </div>
-        </div>
-        <motion.div
-          variants={opacity}
-          animate={isActive ? "closed" : "open"}
-          className={styles.shopContainer}>
-          <FiShoppingCart />
-          <IoIosSearch onClick={toggleSearchBar} />
-          <SearchBar isVisible={isVisible} />
-        </motion.div>
-      </div>
+      )}
+
+      <SearchBar onClick={toggleSearchBar} isVisible={isVisible} />
 
       <AnimatePresence mode="wait">{isShopActive && <ShopMenu />}</AnimatePresence>
       <AnimatePresence mode="wait">{isActive && <BurgerNav />}</AnimatePresence>
--- src/components/header/SearchBar/SearchBar.tsx
+++ src/components/header/SearchBar/SearchBar.tsx
@@ -1,20 +1,41 @@
 import styles from "./SearchBar.module.scss";
 import React from "react";
+import { IoIosClose } from "react-icons/io";
+import { motion } from "framer-motion";
+import { container, item } from "./anim";
 
 interface Props {
   isVisible: boolean;
+  onClick: () => void;
 }
 
-const SearchBar: React.FC<Props> = ({ isVisible }) => {
+const SearchBar: React.FC<Props> = ({ isVisible, onClick }) => {
   return (
-    <div className={styles.searchContainer}>
-      <form className={styles.searchForm} id="search" name="search">
+    <motion.div
+      className={styles.searchContainer}
+      variants={container}
+      initial="hidden"
+      animate="show">
+      <motion.form
+        className={styles.searchForm}
+        id="search"
+        name="search"
+        variants={item}>
         <input
           type="text"
           className={`${styles.searchBar} ${isVisible && styles.visible}`}
           placeholder="Search"></input>
-      </form>
-    </div>
+      </motion.form>
+      {isVisible && (
+        <motion.button
+          className={`${styles.button} ${isVisible && styles.visible}`}
+          onClick={onClick}
+          variants={item}>
+          {" "}
+          <IoIosClose className={styles.icon} />
+        </motion.button>
+      )}
+    </motion.div>
   );
 };
 
