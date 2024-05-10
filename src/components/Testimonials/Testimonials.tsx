55c2d369e3cf6c569524a0c19064236ed51d4c89
kabirseth4 <kabirseth4@gmail.com>
2024-05-10 19:43:17 +0100

    Refactor: convert SchemeSlider styles to tailwind

--- src/components/Testimonials/Testimonials.tsx
+++ src/components/Testimonials/Testimonials.tsx
@@ -10,14 +10,14 @@ const Testimonials = () => {
   });
 
   return (
-    <div className="p-4 pt-8 md:px-12 lg:px-32">
+    <section className="flex w-full flex-col items-center justify-center p-4 pt-8 md:px-12 lg:px-32">
       <h2 className="text-center text-xs font-normal capitalize italic">
         testimonials
       </h2>
       <motion.div
         ref={element}
         style={{ opacity: scrollYProgress }}
-        className="w-1/2 md:flex md:flex-col md:items-start md:justify-center"
+        className="w-3/4 md:flex md:flex-col md:items-start md:justify-center md:self-start"
       >
         <p className="pt-4 text-center text-sm capitalize italic md:text-left">
           a super workshop! well presented with concise instructions and
@@ -27,22 +27,20 @@ const Testimonials = () => {
           mary, norfolk
         </p>
       </motion.div>
-      <div className="w-full text-end md:flex md:items-end md:justify-end">
-        <motion.div
-          ref={element}
-          style={{ opacity: scrollYProgress }}
-          className="w-1/2 md:flex md:flex-col md:items-end md:justify-center"
-        >
-          <p className="pt-4 text-center text-sm capitalize italic md:text-right">
-            the pace and content were exactly right. i feel inspired and will
-            take your advice forward for all future projects.
-          </p>
-          <p className="pt-4 text-center text-xs capitalize italic">
-            rosie, london
-          </p>
-        </motion.div>
-      </div>
-    </div>
+      <motion.div
+        ref={element}
+        style={{ opacity: scrollYProgress }}
+        className="w-3/4 md:flex md:flex-col md:items-end md:justify-center md:self-end"
+      >
+        <p className="pt-4 text-center text-sm capitalize italic md:text-right">
+          the pace and content were exactly right. i feel inspired and will take
+          your advice forward for all future projects.
+        </p>
+        <p className="pt-4 text-center text-xs capitalize italic">
+          rosie, london
+        </p>
+      </motion.div>
+    </section>
   );
 };
 
