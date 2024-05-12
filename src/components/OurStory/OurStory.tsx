909779befc251df810c8c1bf80259eab9ff0d957
kabirseth4 <kabirseth4@gmail.com>
2024-05-12 13:08:42 +0100

    Refactor: convert OurStories styles to tailwind

--- src/components/OurStory/OurStory.tsx
+++ src/components/OurStory/OurStory.tsx
@@ -1,6 +1,5 @@
 "use client";
-import styles from "./OurStory.module.scss";
-import React, { useRef } from "react";
+import { useRef } from "react";
 import { useScroll, motion } from "framer-motion";
 
 const OurStory = () => {
@@ -11,16 +10,17 @@ const OurStory = () => {
   });
 
   return (
-    <div className={styles.bio}>
-      <h2 className={styles.header}>our story</h2>
+    <div className="p-4 pt-8 md:px-12 xl:px-32">
+      <h2 className="text-center text-xs capitalize italic">our story</h2>
       <motion.p
         ref={element}
-        className={styles.text}
-        style={{ opacity: scrollYProgress }}>
+        className="pt-4"
+        style={{ opacity: scrollYProgress }}
+      >
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Est sequi ad
         cumque, aliquid, temporibus maxime culpa nisi similique obcaecati
-        praesentium, deleniti laboriosam voluptas sed facere! Accusantium voluptate
-        laborum deserunt commodi.
+        praesentium, deleniti laboriosam voluptas sed facere! Accusantium
+        voluptate laborum deserunt commodi.
       </motion.p>
     </div>
   );
