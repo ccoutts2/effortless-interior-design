5f6cd8c6b89957a29ff09108068b95bd8e6cd7a6
kabirseth4 <kabirseth4@gmail.com>
2024-05-14 11:25:06 +0100

    Refactor: convert HomeBio styles to tailwind

--- src/components/HomeBio/HomeBio.tsx
+++ src/components/HomeBio/HomeBio.tsx
@@ -1,11 +1,12 @@
-import styles from "./HomeBio.module.scss";
 import FadingDiv from "../FadingDiv/FadingDiv";
 
 const HomeBio = () => {
   return (
-    <div className={styles.bio}>
-      <h2 className={styles.header}>what is an off the peg design scheme?</h2>
-      <FadingDiv className={styles.text}>
+    <div className="p-4 pt-8 md:px-12 xl:px-32">
+      <h2 className="text-center text-xs capitalize italic">
+        what is an off the peg design scheme?
+      </h2>
+      <FadingDiv className="pt-4">
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Est sequi ad
         cumque, aliquid, temporibus maxime culpa nisi similique obcaecati
         praesentium, deleniti laboriosam voluptas sed facere! Accusantium
