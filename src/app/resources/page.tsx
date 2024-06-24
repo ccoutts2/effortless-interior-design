d968ea0c781592825e4f407c7daa7f39122f45df
Chris Coutts <chris.dcoutts@gmail.com>
2024-06-24 16:03:13 +0100

    Add gsap animation to article headers, add lenis scroll to page

--- src/app/resources/page.tsx
+++ src/app/resources/page.tsx
@@ -1,5 +1,7 @@
 "use client";
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
+import gsap from "gsap";
+import Lenis from "lenis";
 
 import Resources from "@/components/Resources/Resources";
 import { PageHeader } from "@/components";
@@ -7,34 +9,57 @@ import { PageHeader } from "@/components";
 const Page = () => {
   const [selectedArticle, setSelectedArticle] = useState("all posts");
 
+  const articles = useRef<(HTMLElement | null)[]>([]);
+
+  const onClick = (article: string, index: number) => {
+    setSelectedArticle(article);
+    gsap.to(articles.current[index], {
+      scale: 1.1,
+      duration: 0.2,
+      ease: "power4.inOut",
+      onComplete: () => {
+        gsap.to(articles.current[index], {
+          scale: 1,
+          duration: 0.2,
+          ease: "power4.inOut",
+        });
+      },
+    });
+  };
+
+  useEffect(() => {
+    const lenis = new Lenis();
+
+    function raf(time: number) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+  }, []);
+
   return (
     <>
       <PageHeader header="resources" />
       <section className="centered flex-col">
         <nav>
-          <ul className="centered cursor-pointer gap-12">
-            <li
-              className={selectedArticle === "all posts" ? "font-bold" : ""}
-              onClick={() => setSelectedArticle("all posts")}
-            >
-              All Posts
-            </li>
-            <li
-              className={selectedArticle === "articles" ? "font-bold" : ""}
-              onClick={() => setSelectedArticle("articles")}
-            >
-              Articles
-            </li>
-            <li
-              className={selectedArticle === "videos" ? "font-bold" : ""}
-              onClick={() => setSelectedArticle("videos")}
-            >
-              Videos
-            </li>
+          <ul className="centered cursor-pointer gap-12 capitalize">
+            {["all posts", "articles", "videos"].map((article, index) => {
+              return (
+                <li
+                  key={index}
+                  className={selectedArticle === article ? "font-bold" : ""}
+                  onClick={() => onClick(article, index)}
+                  ref={(el) => (articles.current[index] = el)}
+                >
+                  {article}
+                </li>
+              );
+            })}
           </ul>
         </nav>
-        <Resources showHeader={false} articleFilter={selectedArticle} />
       </section>
+      <Resources showHeader={false} articleFilter={selectedArticle} />
     </>
   );
 };
