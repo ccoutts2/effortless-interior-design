e6eee3e393428150136573a21b78320c1776b637
Chris Coutts <chris.dcoutts@gmail.com>
2024-06-24 16:48:54 +0100

    Add active page styling and hover styling to navigation links

--- src/components/navigation/NavLink/NavLink.tsx
+++ src/components/navigation/NavLink/NavLink.tsx
@@ -1,5 +1,7 @@
 import Link from "next/link";
 
+import { usePathname } from "next/navigation";
+
 interface NavLinkProps {
   href: string;
   label?: string;
@@ -7,13 +9,19 @@ interface NavLinkProps {
 }
 
 export const NavLink = ({ href, label, onMouseEnter }: NavLinkProps) => {
+  const pathname = usePathname();
+
   return (
     <Link
       className="inline-block w-full py-2 text-sm uppercase lg:w-fit lg:py-3 lg:text-lg"
       href={href}
       onMouseEnter={onMouseEnter}
     >
-      {href || label}
+      <span
+        className={`hover:border-b-2 hover:border-[#5d3a40] ${pathname === href ? "border-b-2 border-[#5d3a40]" : ""}`}
+      >
+        {label}
+      </span>
     </Link>
   );
 };
