d2b5887461c3c1da3ebaf1ae4d0680d7ef84804e
Chris Coutts <chris.dcoutts@gmail.com>
2024-06-25 11:25:42 +0100

    Add js-cookie package and set up basic cookie functionality accept/request

--- src/components/Cookie/Cookie.tsx
+++ src/components/Cookie/Cookie.tsx
@@ -2,6 +2,7 @@
 
 import { useState, useEffect, useRef } from "react";
 import { Button } from "@/components";
+import Cookies from "js-cookie";
 import Link from "next/link";
 
 const Cookie = () => {
@@ -9,17 +10,20 @@ const Cookie = () => {
   const [consent, setConsent] = useState(false);
 
   useEffect(() => {
+    const consent = Cookies.get("cookieConsent");
     if (!consent) {
       setShowCookie(true);
     }
   }, []);
 
   const acceptCookie = () => {
+    Cookies.set("cookieConsent", "true", { expires: 365 });
     setConsent(true);
     setShowCookie(false);
   };
 
   const rejectCookie = () => {
+    Cookies.set("cookieConsent", "false", { expires: 365 });
     setConsent(false);
     setShowCookie(false);
   };
@@ -29,7 +33,7 @@ const Cookie = () => {
   return (
     <>
       {showCookie && (
-        <div className="fixed bottom-4 left-0 mx-4 flex max-w-sm flex-col items-start justify-start gap-4 bg-[#744442] p-2 text-[#f3eee8]">
+        <div className="fixed bottom-4 left-0 mx-4 flex max-w-sm flex-col items-start justify-start gap-4 bg-[#744442] p-2 text-[#f3eee8] md:gap-12">
           <div>
             <p className="text-sm">
               We use some essential cookies to ensure you get the best
@@ -39,12 +43,12 @@ const Cookie = () => {
               </span>
             </p>
           </div>
-          <div className="items-between flex w-full flex-col gap-4">
-            <div className="centered border border-[#f3eee8]">
-              <Button label="accept all" onClick={acceptCookie} />
-            </div>
+          <div className="items-between flex w-full flex-col gap-4 md:flex-row md:justify-between">
             <div className="centered border border-[#f3eee8]">
               <Button label="reject all" onClick={rejectCookie} />
+            </div>
+            <div className="centered border border-[#f3eee8]">
+              <Button label="accept all" onClick={acceptCookie} />
             </div>
           </div>
         </div>
--- src/components/buttons/Button/Button.tsx
+++ src/components/buttons/Button/Button.tsx
@@ -1,6 +1,6 @@
 interface ButtonProps {
   label: string;
-  onClick: () => void;
+  onClick?: () => void;
 }
 
 export const Button = ({ label, onClick }: ButtonProps) => {
