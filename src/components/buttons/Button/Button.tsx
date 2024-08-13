fc9032179e28283cfd45fc61cdd29a4e2e88c31d
Chris Coutts <chris.dcoutts@gmail.com>
2024-08-13 20:09:04 +0100

    Refactor: extend props from HTMLButtonElement

--- src/components/buttons/Button/Button.tsx
+++ src/components/buttons/Button/Button.tsx
@@ -1,13 +1,14 @@
-interface ButtonProps {
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   label: string;
   onClick?: () => void;
 }
 
-export const Button = ({ label, onClick }: ButtonProps) => {
+export const Button = ({ label, onClick, ...defaultProps }: ButtonProps) => {
   return (
     <button
       className="mx-0 my-[0.75rem] h-auto w-auto bg-[#744442] px-[1.8rem] py-2 text-xs uppercase tracking-[1px] text-[#f3eee8]"
       onClick={onClick}
+      {...defaultProps}
     >
       {label}
     </button>
