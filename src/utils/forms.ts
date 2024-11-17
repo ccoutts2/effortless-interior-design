90bbc7d76444947afa3e37c536fc8b572b8e026d
Chris Coutts <chris.coutts@sunderlandsoftwarecity.com>
2024-11-17 13:57:54 +0000

    Removed unused zod error utils file

--- src/utils/forms.ts
+++
@@ -1,8 +0,0 @@
-import { ZodError } from "zod";
-
-export const convertZodErrors = (error: ZodError) => {
-  return error.issues.reduce((acc: { [key: string]: string }, issue) => {
-    acc[issue.path[0]] = issue.message;
-    return acc;
-  }, {});
-};
